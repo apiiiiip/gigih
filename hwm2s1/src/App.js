df36d7de69a4bab2f8f6f6c4f4ffe3fbfb60b298
apiiiiip <apip.afif46@gmail.com>
2022-03-23 13:30:09 +0700

    perubahan hwm2s1

--- hwm2s1/src/App.js
+++ hwm2s1/src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import {data} from './data.js';
 
 function App() {
   return (
@@ -10,9 +11,9 @@ function App() {
         <div className="App-main">
           <p className="App-mainTitle">Song Info</p>
           <div className="App-mainTrack">
-            <img src ="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" className="App-mainImg"/>
+            <img src = "https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" className="App-mainImg"/>
             <p className="App-mainDesc">
-              <span className="App-mainSong">Bohemian Rhapsody (The Original Soundtrack)</span><br></br>
+              <span className="App-mainSong">{data.album.name}</span><br></br>
               <span className="App-mainArtist">Queen</span>  
             </p>
             <button className="btn btn-select">Select</button>
@@ -22,5 +23,6 @@ function App() {
     </body>
   );
 }
-
+console.log(data.album);
+console.log(data.artists);
 export default App;
